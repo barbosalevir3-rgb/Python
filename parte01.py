1910dcbe1722c8fc9dbfd16f6ab724d6f71c5462
Levir Barbosa <barbosalevir3@gmail.com>
2026-09-13 12:09:07 -0400

    Create parte01.py

---
+++ parte01.py
@@ -0,0 +1,15 @@
+// Uso do type():
+// informa o tipo de dado armazenado na variável.
+// str   → texto
+// int   → número inteiro
+// float → número decimal
+// bool  → verdadeiro/falso
+
+nome = "Levir";
+idade = 19;
+altura = 1.75;
+ativo = True;
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(ativo))
